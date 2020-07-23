ea2bbb29ef59b13c922e2be5e0fbfcbf37dca589
Anup Kale <anup.kale@itexecutors.com>
2020-07-23 20:11:03 +0530

    Basic JavaScript overview project. Initial Commit

---
+++ assets/script/main.js
@@ -0,0 +1,36 @@
+const backdropElement = document.getElementById('backdrop');
+const modalLinkElements = document.querySelectorAll('.example-link');
+let infoModal;
+
+function toggleBackdrop() {
+    backdropElement.classList.toggle('visible');
+}
+
+function presentInfoModal(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const text = event.target.dataset.text;
+    toggleBackdrop();
+    infoModal = document.createElement('div');
+    infoModal.classList.add('modal');
+    infoModal.innerHTML = `
+    <h2>More Details</h2>
+    <p>${text}</p>
+  `;
+    const closeButton = document.createElement('button');
+    closeButton.addEventListener('click', hideInfoModal);
+    closeButton.textContent = 'Okay';
+    infoModal.appendChild(closeButton);
+    document.body.appendChild(infoModal);
+}
+
+function hideInfoModal() {
+    toggleBackdrop();
+    document.body.removeChild(infoModal);
+}
+
+for (const linkElement of modalLinkElements) {
+    linkElement.addEventListener('click', presentInfoModal);
+}
+
+backdropElement.addEventListener('click', hideInfoModal);
